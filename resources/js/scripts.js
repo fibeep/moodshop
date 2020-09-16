8c341d344c37f72b3fb93544fcd7da1753f21936
Salo <salomonco@gmail.com>
2020-09-16 19:13:27 -0400

    moods + buttons displayd w/ JS

---
+++ resources/js/scripts.js
@@ -0,0 +1,41 @@
+const itemsContainer = document.getElementById('items')
+
+import data from './data.js'
+
+// the length of our data determines how many times this loop goes around
+for (let i = 0; i < data.length; ++i) {
+    // create a new div element and give it a class name
+    let newDiv = document.createElement('div');
+    newDiv.className = 'item'
+
+    // create an image element
+    let img = document.createElement('img');
+    // this will change each time we go through the loop. Can you explain why?
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+
+    // Add the image to the div
+    newDiv.appendChild(img)
+    itemsContainer.appendChild(newDiv)
+
+    // Add Description
+    let descript = document.createElement('P')
+    descript.innerText = data[i].desc
+    newDiv.appendChild(descript)
+
+    // Add Price
+    let price = document.createElement('P')
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+
+    // Add Button
+    let button = document.createElement('button')
+    button.id = data[i].name
+
+    // Custom Data Attributes
+
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+}
