38880ae41d0ccb7fd683b11b330b33274489c2f3
Salo <salomonco@gmail.com>
2020-09-20 18:12:52 -0400

    cart fully functional, bug fixed

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -46,21 +46,47 @@ for (let i = 0; i < data.length; ++i) {
     button.style.borderColor = "blue"
     button.style.fontFamily = "comic sans"
     newDiv.appendChild(button)
-
-    // Query Select All Buttons
-    const all_items_button = Array.from(document.querySelectorAll("button"))
-
-    // For Each Loop
-    
-
-    all_items_button.forEach(elt => elt.addEventListener('click', () => {
-    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
-    showItems()
-}))
-
+  
 }
+    // Query Select All Buttons
+
+const all_items_button = document.querySelectorAll("button")
+    // For Each Loop
+
+all_items_button.forEach((element) => {
+    element.addEventListener("click", () => {
+        addItem(element.getAttribute("id"), element.getAttribute("data-price"))
+        showItems()
+    })
+})
 
 const cart = []
+
+//Handle change events on update input
+
+itemList.onchange = function(e) {
+    if (e.target && e.target.classList.contains('update')) {
+        const name = e.target.dataset.name
+        const qty = parseInt(e.target.value)
+        updateCart(name, qty)
+    }
+}
+
+//Handle clicks on list
+
+itemList.onclick = function(e) {
+    if (e.target && e.target.classList.contains('remove')){
+        const name = e.target.dataset.name
+        removeItem(name)
+    } else if (e.target && e.target.classList.contains("add-one")) {
+    const name = e.target.dataset.name
+    addItem(name)
+    } else if (e.target && e.target.classList.contains("remove-one")) {
+        const name = e.target.dataset.name
+        removeItem(name, 1)
+    }
+
+}
 
 //Add Item
 function addItem(name, price) {
@@ -90,7 +116,12 @@ function showItems() {
         
         itemStr += `<li> ${name} 
         $${price} x ${qty}
-        = ${price * qty}</li>`
+        = ${price * qty}
+        <button class="remove" data-name="${name}">Remove</button>
+        <button class="add-one" data-name="${name}">+</button>
+        <button class="remove-one" data-name="${name}">-</button>
+        <input class="update" type="number" min="0" data-name=${name}>
+        </li>`;
     }
 
     itemList.innerHTML = itemStr
@@ -127,6 +158,23 @@ function removeItem (name, qty = 0) {
             if (cart[i].qty < 1 || qty === 0) {
                 cart.splice(i, 1)
             }
+            showItems()
+            return
+        }
+    }
+}
+
+//Update Cart Function
+
+function updateCart(name, qty) {
+    for(let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            if (qty < 1) {
+                removeItem(name)
+                return
+            }
+            cart[i].qty = qty
+            showItems()
             return
         }
     }
